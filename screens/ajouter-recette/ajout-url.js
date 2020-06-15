b096cf9dcd35fdf60052e36b9eec1d10675ae0c3
Eva Delpierre <eva.delpierre@gmail.com>
2020-06-15 19:51:41 +0200

    fuck

--- screens/ajouter-recette/ajout-url.js
+++ screens/ajouter-recette/ajout-url.js
@@ -3,41 +3,26 @@ import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
 import { Input, Button } from 'react-native-elements';
 import FlecheRetour from '../../assets/images/icones/fleche-retour.svg';
 import { connect } from 'react-redux';
-import PlaceHolderAccueil from '../../components/PlaceHolderAccueil'
-
 
 function ImportUrl({navigation, sendUrlToRedux}) {
 
     const [isEnabled, setIsEnabled] = useState(false);
     const [urlMarmiton, setUrlMarmiton] = useState('');
-    const [isFetching, setIsFetching] = useState(null)
-
-
     const toggleSwitch = () => setIsEnabled(previousState => !previousState);
 
     var addUrl = async () => {
-        setIsFetching(true)
         const url = await fetch('http://192.168.1.62:3000/saveMarmiton', {
           method:"POST",
           headers : {'Content-Type': 'application/x-www-form-urlencoded'},
           body: `lienMarmiton=${urlMarmiton}`
         });
         const newRecette = await url.json();
-        console.log(newRecette)
+        
         if(newRecette){
-            setIsFetching(false)
             sendUrlToRedux(newRecette);
             navigation.navigate('Recap');
         }
     }
-
-    if(isFetching){
-        return (
-            <PlaceHolderAccueil />
-        )
-    } else {
-
-   
     
     return (
 
@@ -76,7 +61,6 @@ function ImportUrl({navigation, sendUrlToRedux}) {
             </SafeAreaView>
         </View>
     )
-}
 }
 
 
