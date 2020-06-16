fcd06b0626e48d332bd9261116d504cb16883f96
Sebastian Danero <sebastian.danero@gmail.com>
2020-06-16 09:29:17 +0200

    test

--- components/PlaceHolderAccueil.js
+++ components/PlaceHolderAccueil.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Placeholder, PlaceholderLine, PlaceholderMedia, ShineOverlay } from 'rn-placeholder'
+import { Placeholder, PlaceholderLine, PlaceholderMedia, ShineOverlay ,Fade, Shine} from 'rn-placeholder'
 import { responsiveHeight, responsiveWidth } from 'react-native-responsive-dimensions'
 import { View } from 'react-native'
 
@@ -7,38 +7,28 @@ function PlaceHolderAccueil() {
 
 
     return (
-        <View style={{marginTop: 200}}>
-            <Placeholder
-                Animation={ShineOverlay}
+        <View style={{ flex: 1, backgroundColor: "#FF5A5B" }}>
+            <Placeholder Animation={Fade}
                 style={{
                     marginVertical: 6,
-                    marginHorizontal: 15,
                     borderRadius: 4,
-                }}
-                Left={props => (
-                    <PlaceholderMedia
-                        style={[
-                            props.style,
-                            {
-                                width: responsiveWidth(35),
-                                height: responsiveHeight(16)
-                            }
-                        ]}
-                    />
-                )}
-                Right={props => (
-                    <PlaceholderMedia
-                        style={[
-                            props.style,
-                            {
-                                width: responsiveWidth(30),
-                                height: responsiveHeight(16)
-                            }
-                        ]}
-                    />
-                )}
-            >
-                <PlaceholderMedia style={{width: responsiveWidth(30), height: responsiveHeight(16)}} />
+                    marginTop: 200
+                }}>
+                <PlaceholderLine width={100} height={30} style={{backgroundColor: "#FF5A5B"}} />
+                <View style={{flexDirection: "row", justifyContent: "space-between", paddingLeft: 15, paddingRight: 15}}>
+                    <PlaceholderLine width={30} height={100} style={{backgroundColor: "#FFC830"}} />
+                    <PlaceholderLine width={30} height={100} style={{backgroundColor: "#FFC830"}}/>
+                    <PlaceholderLine width={30} height={100} style={{backgroundColor: "#FFC830"}}/>
+                </View>
+                <View style={{justifyContent: "flex-end", alignItems: "flex-end"}}>
+                    <PlaceholderLine width={30} height={30} />
+                </View>
+                <View style={{alignItems: "center"}}>
+                    <PlaceholderLine width={80} height={50} />
+                </View>
+                <View style={{alignItems: "center"}}>
+                    <PlaceholderLine width={50} height={50} style={{backgroundColor: "#FFC830"}} />
+                </View>
             </Placeholder>
         </View>
     )
